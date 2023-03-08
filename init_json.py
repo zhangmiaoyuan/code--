7823e7a18db8eb9c24501bb2c6f469e2379894f6
zhangmiaoyuan <122366663+zhangmiaoyuan@users.noreply.github.com>
2023-03-08 12:57:45 +0800

    Add files via upload

---
+++ init_json.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import json
+# my_dict = {}
+# #my_dict = dict()
+# my_dict1 = {"王力宏": 99 ,"周杰伦": 88 ,"林俊杰": 77}
+# score = my_dict1["王力宏"]
+# print(score)
+# my_dict1["张学友"] = 66
+# score = my_dict1["张学友"]
+# print(score)
+# my_dict = {}
+# hight_wight{}
+# my_dict[image_name] = {hight_wight[hight] = hight_truth,hight_wight[wignt] = wight_truth}
+image_name = {}
+image_name["H"] = height
+image_name["W"] = width
+
+image_name["box_examples_path"] = box_list
+
+image_name["density_path"] = box_list
+
+image_name["density_path_fixed"] = box_list
+
+image_name["img_path"] = box_list
+image_name["points"] = point_list
+
+image_name["r"] = box_list
+image_name["ratio_h"] = box_list
+image_name["ratio_w"] = box_list
+
+final_json = {}
+final_json["image_name"] = image_name
